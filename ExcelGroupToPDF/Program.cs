9d52d0e2f1b4bae05aae999e157a5463fecd3a56
Marcelo <mrangel@itexico.com>
2020-03-10 18:52:18 -0600

    width percentages, Headers in bold

--- ExcelGroupToPDF/Program.cs
+++ ExcelGroupToPDF/Program.cs
@@ -15,7 +15,7 @@ using iText.Layout.Element;
 using iText.Kernel.Geom;
 using iText.IO.Font.Constants;
 using Path = System.IO.Path;
-
+using System.Globalization;
 
 namespace ExcelGroupToPDF
 {
@@ -42,6 +42,27 @@ namespace ExcelGroupToPDF
 
             // var memberValue = (string)(exWks.Cells[4, 5] as Microsoft.Office.Interop.Excel.Range).Value;
             //Microsoft.Office.Interop.Excel.Range groupedRange;
+
+            MemoryRow memoryHeaders = new MemoryRow()
+            {
+                CustNo = "Cust No",
+                ShipTo = "Ship To",
+                PONO = "PO No.",
+                CSR = "CSR",
+                SLSNAME = "SLS Name",
+                OrderNo = "Order No.",
+                ReleaseNo = "Release No.",
+                OrdDate = "Ord. Date",
+                PromiseDate = "Promise Date",
+                ItemNo = "Item No.",
+                CustItemNo = "Cust. Item No.",
+                CustDescrip = "Cust. Description",
+                WHSE = "WHSE",
+                OrdQty = "Ord. Qty",
+                OrdAvailQty = "Ord. Avail. Qty.",
+                HoldTerms = "Hold Terms"
+            };
+
             List<MemoryRow> objs =  assignProperties(xlRange);
 
             var groupedFields = (from o in objs
@@ -50,112 +71,86 @@ namespace ExcelGroupToPDF
 
             foreach (var custNoGroup in groupedFields)
             {
-                /****
-                Worksheet newWorkSheet = exWbk.Sheets.Add(Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-                Range groupRange = newWorkSheet.get_Range("A1", "P" + custNoGroup.Count()  );
-
                 
-                string[,] arrayGroup = new string[custNoGroup.Count(), NumberOfColumns];
-
-                int i = 0;
-                foreach (var row in custNoGroup)
-                {
-                    arrayGroup[i, 0] =  row.CustNo;
-                    arrayGroup[i, 1] =  row.ShipTo;
-                    arrayGroup[i, 2] =  row.PONO;
-                    arrayGroup[i, 3] =  row.CSR;
-                    arrayGroup[i, 4] =  row.SLSNAME;
-                    arrayGroup[i, 5] =  row.OrderNo;
-                    arrayGroup[i, 6] =  row.ReleaseNo;
-                    arrayGroup[i, 7] =  row.OrdDate;
-                    arrayGroup[i, 8] =  row.PromiseDate;
-                    arrayGroup[i, 9] = row.ItemNo;
-                    arrayGroup[i, 10] = row.CustItemNo;
-                    arrayGroup[i, 11] = row.CustDescrip;
-                    arrayGroup[i, 12] = row.WHSE;
-                    arrayGroup[i, 13] = row.OrdQty;
-                    arrayGroup[i, 14] = row.OrdAvailQty;
-                    arrayGroup[i, 15] = row.HoldTerms;
-                    i++;
-                  //  groupRange.Insert(Type.Missing, arrayGroup);
-                  groupRange.Value = arrayGroup;
-                }****/
-                //cell.SetBorder(Border.NO_BORDER);
-
-                //groupRange.ExportAsFixedFormat
                 // Save into a PDF.
                 #region savepdf
 
                 PdfDocument pdfDoc = new PdfDocument(new PdfWriter("_ALP_OpenOrderCSR_TEST_" + custNoGroup.Key + ".pdf"));
                 Document doc = new Document(pdfDoc, PageSize.LEGAL.Rotate());
                 PdfFont font = PdfFontFactory.CreateFont(StandardFonts.TIMES_ROMAN);
-                Table table = new Table(UnitValue.CreatePercentArray(new float[] { 5, 5, 5, 5, 5, 5, 5, 20, 1, 1, 1, 1, 1, 5, 5, 5 })).UseAllAvailableWidth();
+                Table table = new Table(UnitValue.CreatePercentArray(new float[] { 13, 7, 5, 9, 4, 3, 4, 4, 4, 13, 18, 2, 5, 6, 3 })).UseAllAvailableWidth();
+
+                table.AddCell(new Cell().Add (new Paragraph("Alpha Open Orders Report (CSR)")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
+                table.StartNewRow();
+                table.AddCell(new Cell().Add(new Paragraph("Customer: " + custNoGroup.Key)).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
+                table.StartNewRow();
                 
+                table.StartNewRow();
+                table = CreateTableRow(table, memoryHeaders, true);
+
                 foreach (var row in custNoGroup)
                 {
-                     table.AddCell(new Cell().Add(new Paragraph(row.CustNo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.ShipTo??"")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.PONO ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.CSR ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.SLSNAME ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.OrderNo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.ReleaseNo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.OrdDate ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.PromiseDate ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.ItemNo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.CustItemNo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.CustDescrip ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.WHSE ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.OrdQty ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.OrdAvailQty ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                     table.AddCell(new Cell().Add(new Paragraph(row.HoldTerms ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER));
-                    //cell.SetTextAlignment(TextAlignment.CENTER);
-                    table.StartNewRow();
+                    table = CreateTableRow(table, row);
                 }
-                table.SetFont(font).SetFontSize(8);
+                table.SetFontSize(8);
                 doc.Add(table);
                 doc.Close();
 
-                /*
-                document.SetMargins(20, 20, 20, 20);
-                Table table = new Table(UnitValue.CreatePercentArray(new float[] { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 })).UseAllAvailableWidth();
-                document.Add(new Paragraph("Testing"));
-                //document.Add(table);
-                document.Close();
-                result = memoryStream.ToArray();
-               
-    */
-
-                //table.SetWidth(UnitValue.CreatePercentValue(100));
-                /*StreamReader sr = File.OpenText(DATA);
-                var line = sr.ReadLine();
-                process(table, line, bold, true);
-                while ((line = sr.ReadLine()) != null)
-                {
-                    process(table, line, font, false);
-                }
-                sr.Close();
-                document.Add(table);
-                document.Close();*/
+                exApp.Workbooks.Close();
                 #endregion
-
-
-
-
-
-
-
-                /*
-                string filename = "CustomerInfo_" + custNoGroup.Key + ".pdf";
-                const int xlQualityStandard = 0;
-                //exWks.ExportAsFixedFormat(
-                groupRange.ExportAsFixedFormat(
-                    Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF,
-                    filename, xlQualityStandard, true, true,
-                    Type.Missing, Type.Missing, true, Type.Missing);
-                    */
             }
         }
+
+        public static Table CreateTableRow(Table table, MemoryRow row, bool isHeader = false )
+        {
+            PdfFont font = PdfFontFactory.CreateFont(StandardFonts.TIMES_ROMAN);
+            if (isHeader)
+            {
+                font = PdfFontFactory.CreateFont(StandardFonts.TIMES_BOLD);
+            }
+
+            table.AddCell(new Cell().Add(new Paragraph(row.ShipTo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.PONO ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.CSR ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.SLSNAME ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.OrderNo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.ReleaseNo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+
+            string ordDate = "";
+            string promiseDate = "";
+            if (isHeader)
+            {
+                ordDate = row.OrdDate ?? "";
+                promiseDate = row.PromiseDate ?? "";
+            }
+            else
+            {
+                CultureInfo culture = new CultureInfo("en-US");
+                if (!string.IsNullOrEmpty(row.OrdDate))
+                {
+                    ordDate = Convert.ToDateTime(row.OrdDate, culture).ToShortDateString();
+                }
+
+                if (!string.IsNullOrEmpty(row.PromiseDate))
+                {
+                    promiseDate = Convert.ToDateTime(row.PromiseDate, culture).ToShortDateString();
+                }
+            }
+
+            table.AddCell(new Cell().Add(new Paragraph(ordDate)).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(promiseDate)).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.ItemNo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.CustItemNo ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.CustDescrip ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.WHSE ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.OrdQty ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.OrdAvailQty ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+            table.AddCell(new Cell().Add(new Paragraph(row.HoldTerms ?? "")).SetBorder(iText.Layout.Borders.Border.NO_BORDER).SetFont(font));
+
+            table.StartNewRow();
+            return table;
+        }
+
 
         public static List<MemoryRow>  assignProperties(Range xlRange)
         {
@@ -165,26 +160,7 @@ namespace ExcelGroupToPDF
             int firstColumn = 1;
             int firstRow = 1;
             long lastRow = firstRow + rows - 1;
-            MemoryRow memoryHeaders = new MemoryRow()
-            {
-                CustNo =        "CUSTNO",
-                ShipTo =        "SHIP_TO",
-                PONO =          "PONO",
-                CSR =           "CSR",
-                SLSNAME =       "SLSNAME",
-                OrderNo =       "ORDERNO",
-                ReleaseNo =     "RELEASE_NO",
-                OrdDate =       "ORD_DATE",
-                PromiseDate =   "PROMISE_DATE",
-                ItemNo =        "ITEMNO",
-                CustItemNo =    "CUST_ITEMNO",
-                CustDescrip =   "CUST_DESCRIP",
-                WHSE =          "WHSE",
-                OrdQty =        "ORD-QTY",
-                OrdAvailQty =   "ORD-AVAIL-QTY",
-                HoldTerms =     "HOLD Terms"
-            };
-            objs.Add(memoryHeaders);
+           
 
             //get Header row
             var nulvalue = Convert.ToString((xlRange.Cells[4, 5]).Value);
